a24fe652064118cc810c0a453ed7fad90161b5c5
ManukMelkonyan <myan.manuk@gmail.com>
2022-07-30 23:26:20 +0400

    feature/started working on array parsing functions

--- interpreter.js
+++ interpreter.js
@@ -8,9 +8,22 @@ class Expression{
     }
 }
 
+class IndexerLiteral {
+    constructor(name, argument) {
+        this.name = name;
+        this.argument = argument;
+    }
+}
+
+class ArrayLiteral {
+    constructor() {
+
+    }
+}
+
 class Statement{
     constructor(){
-
+        this.type = this.constructor.name;
     }
 }
 
@@ -62,7 +75,6 @@ class Interpreter{
     checkStringSyntax(code){
 
     }
-    
 
     isValidIdentifier(name){
         return /^[a-zA-Z_$]+[a-zA-Z_$0-9]*/g.test(name);
@@ -94,10 +106,10 @@ class Interpreter{
                 throw new Error(`Invalid identifier name: ${name}`);
             }
             else{
-                let args = this.getParameters();
+                let params = this.getParameters();
                 let bodyStatements = this.getBlockStatements();
 
-                statement = new FunctionStatement(name, args, bodyStatements);
+                statement = new FunctionStatement(name, params, bodyStatements);
             }
         }
         else if(first === 'if'){
@@ -131,10 +143,15 @@ class Interpreter{
             //     let expressionTokens = this.getExpressionTokens().unshift(name);
             //     statement = new Expression(expressionTokens);
             // }
-            else if(nextToken === '('){
+            else if(nextToken === '(') {
                 this.components.shift();
                 let args = this.getArguments();
                 statement = new FunctionCall(name, args);
+            }
+            else if(nextToken === '[') {
+                this.components.shift();
+                let arg = this.getIndexerArgument();
+                statement = new IndexerLiteral(name, arg);
             }
         }
         else if(!this.isValidIdentifier(first)){
@@ -158,6 +175,47 @@ class Interpreter{
         }
         // console.log(this.components);
         return tokens;
+    }
+
+    getIndexerArgument() {
+        let bracketStack = [-1];
+        let args = [];
+        if(this.components[0] === ']'){
+            this.components.shift();
+            throw new Error('Invalid syntax. Indexer value is missing');
+        }
+        let currentIndexerTokens = [];
+        let prev = null;
+        let current = null;
+        while(this.components.length){
+            prev = current;
+            current = this.components[0];
+            if(current === ']'){
+                bracketStack.pop();
+                this.components.shift();
+                if (bracketStack.length){
+                    currentIndexerTokens.push(current);
+                }
+            }
+            else if(current === '[') {
+                bracketStack.push(-1);
+                currentIndexerTokens.push(current);
+                this.components.shift();
+            }
+            else{
+                currentIndexerTokens.push(current);
+                this.components.shift();
+            }
+            
+            if(bracketStack.length === 0){
+                if(currentIndexerTokens.length){
+                    const currentExpression = new Expression(currentIndexerTokens);
+                    args.push(currentExpression);
+                }
+                return args;
+            }
+        }
+
     }
 
     getArguments(){
@@ -223,6 +281,71 @@ class Interpreter{
             }
         }
     }
+
+    getArrayElements(){
+        let bracketStack = [-1];
+        let args = [];
+        if(this.components[0] === ','){
+            throw new Error(`Invalid syntax. Argument must be passed before ','`);
+        }
+        if(this.components[0] === ')'){
+            this.components.shift();
+            return args;
+        }
+        let currentArgumentTokens = [];
+        let prev = null;
+        let current = null;
+        while(this.components.length){
+            prev = current;
+            current = this.components[0];
+            if(current === ')'){
+                bracketStack.pop();
+                this.components.shift();
+                if(bracketStack.length){
+                    currentArgumentTokens.push(current);
+                }
+            }
+            else if(current === '('){
+                if(this.isValidIdentifier(prev)){
+                    this.components.shift();
+                    current = new FunctionCall(prev, this.getArguments());
+                    currentArgumentTokens.pop();
+                    currentArgumentTokens.push(current);
+                }
+                else{
+                    bracketStack.push(-1);
+                    currentArgumentTokens.push(current);
+                    this.components.shift();
+                }
+            }
+            else if(current === ','){
+                if(currentArgumentTokens.length === 0){
+                    throw new Error(`Invalid syntax. Argument must be passed before ','`);
+                }
+                else{
+                    if(bracketStack.length === 1){
+                        const currentExpression = new Expression(currentArgumentTokens);
+                        args.push(currentExpression);
+                        currentArgumentTokens = [];
+                    }
+                    this.components.shift();
+                }
+            }
+            else{
+                currentArgumentTokens.push(current);
+                this.components.shift();
+            }
+            
+            if(bracketStack.length === 0){
+                if(currentArgumentTokens.length){
+                    const currentExpression = new Expression(currentArgumentTokens);
+                    args.push(currentExpression);
+                }
+                return args;
+            }
+        }
+    }
+
 
     getParameters(){
         if(this.components.shift() !== '('){
@@ -321,14 +444,56 @@ class Interpreter{
     }
 }
 
+const sampleParser = (expr) => {
+    let ops = {
+        '+': { priority: 0, index: -1 },
+        '-': { priority: 0, index: -1 },
+        '*': { priority: 1, index: -1 },
+        '/': { priority: 1, index: -1 },
+    }
+    let currentPriority = Infinity;
+    let currentOp = null;
+    let currentOpIndex = -1;
+
+    let left = '';
+    let right = '';
+    for(let i = 0; i < expr.length; ++i) {
+        const char = expr[i];
+
+        if (char in ops) {
+            const { priority, index } = ops[char];
+            if (priority < currentPriority) {
+                currentPriority = priority;
+                currentOp = char;
+                currentOpIndex = i;
+            }
+        }
+    }
+    if (currentOp) {
+        left = expr.slice(0, currentOpIndex);
+        right = expr.slice(currentOpIndex + 1, expr.length);
+    }
+    
+    if(left && right) {
+        console.log({ currentPriority, left, currentOp, right })
+        console.log('='.repeat(100));
+        sampleParser(left);
+        sampleParser(right);
+    }
+}
+
 let Urartu = new Interpreter();
+// console.log('5+8*6+7/1-3');
+// sampleParser('5+8*6+7/1-3');
 // Urartu.run(`
 //     x = 5 + 8 * (c - l);
+//     y = [1, 2, 3];
 // `);
 Urartu.run(`
     x = 5;
     y = 8;
-    foo(1, a, a + b(0, 'f(', f(), t), c);
+    x[1];
+    foo(1, a, a + b(0, 'f(', f(), t), c)
     if(k + l){
         k = 10;
         l = 66;
@@ -338,7 +503,7 @@ Urartu.run(`
         ;
         p = " ' fasf ";
     }
-`)
+`);
 
-
+// console.log(JSON.stringify(Urartu.memory['main'], null, 4));
 console.log(Urartu.memory['main']);
